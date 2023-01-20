cd7f6fa3ee4e30bc509d48f995ee2de536f321fc
Dmitrii <sutyagin15086733@yandex.ru>
2023-01-20 18:34:56 +0300

    Решение четвертой задачи

--- Seminar_2/Task_4.py
+++ Seminar_2/Task_4.py
@@ -4,24 +4,16 @@
 # Какова вероятность того, что хотя бы один мяч белый?
 from math import factorial
 import numpy as np
-
-
 def C ( n, k):
     return np.math.factorial(n) //( np.math.factorial(k) * np.math.factorial(n - k))
 m = C(16, 4)
 n = C(21, 4)
 Pa = m / n
 print(f'Вероятность того, что все мячи белые = {round(Pa*100, 2)}%')
-
 m = C(16, 2)
 n = C(21, 4)
 Pa = m / n
 print(f'Вероятность того, что ровно два мяча белые = {round(Pa*100, 2)}%')
-
-
-
-
-
 l = sum([C(4,1)*C(17, 3), C(4,2)*C(17, 2),C(4,3)*C(17,1), 1])
 Pa = l / n
-print(f'Вероятность того, что хотя бы один мяч белый = {round(Pa, 4)}')
+print(f'Вероятность того, что хотя бы один мяч белый = {round(Pa*100, 2)}%')
